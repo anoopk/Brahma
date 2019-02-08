c5bb9a939e339b4770ac40111de3852cc83db292
anoopk <anoopk@alohatechnology.com>
2019-02-09 03:20:42 +0530

    New input files. Collection names with metadata corrected.  Database name for 'Reviews'  corrected.

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 var lr = require('line-reader');
 
-//var reviewanalysis = require('./src/sources/reviews/analysis_api_input');
-//var wikianalysis = require('./src/sources/wiki/analysis_api_input');
-var us = require('./lib/upstream.js');
+var reviewanalysis = require('./lib/sources/reviews/analysis_api_input');
+var wikianalysis = require('./lib/sources/wiki/analysis_api_input');
+//var us = require('./lib/upstream.js');
 
-var obj = {"sentiment":.54, "organization":"Maruti", "product":"WagonR", "analysis":"sentiment", "reviews": 3, timestamp: { type: Date, default: Date.now}};
+//var obj = {"sentiment":.54, "organization":"Maruti", "product":"WagonR", "analysis":"sentiment", "reviews": 3, timestamp: { type: Date, default: Date.now}};
 //us.analyse("Statistics", obj);
 
-var newobj = {"sentiment":.45, "organization":"Maruti", "product":"WagonR", "analysis":"sentiment"};
-us.aggregate("Statistics", newobj);
+//var newobj = {"sentiment":.45, "organization":"Maruti", "product":"WagonR", "analysis":"sentiment"};
+//us.aggregate("Statistics", newobj);
 //us.drop("Statistics", "Maruti/WagonR");
-//reviewanalysis.analyse("input/inputURLList.txt");
-//wikianalysis.analyse("input/inputWikiList.txt");
+
+reviewanalysis.analyse("input/inputUrlList.txt");
+wikianalysis.analyse("input/inputWikiList.txt");
