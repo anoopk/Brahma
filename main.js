7948530affd088de1e68f285eefba4bbb1bcbb41
anoopk <anoopk@alohatechnology.com>
2019-02-09 01:47:22 +0530

    src renamed to lib

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var lr = require('line-reader');
 
 //var reviewanalysis = require('./src/sources/reviews/analysis_api_input');
 //var wikianalysis = require('./src/sources/wiki/analysis_api_input');
-var us = require('./src/upstream.js');
+var us = require('./lib/upstream.js');
 
 var obj = {"sentiment":.54, "organization":"Maruti", "product":"WagonR", "analysis":"sentiment", "reviews": 3, timestamp: { type: Date, default: Date.now}};
 //us.analyse("Statistics", obj);
