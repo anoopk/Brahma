da3d9d5d9f4a0ff043ce4580de1f7ca4fa9027dd
Anoop Kumar <anoop_kr@yahoo.com>
2019-02-11 18:43:13 +0530

    Halfway to upstream calculations.

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 var lr = require('line-reader');
 
 const config = require('./lib/config.json');
-console.log(config);
+//console.log(config);
  
 var reviewanalysis = require('./lib/sources/reviews/analysis_api_input');
 reviewanalysis.analyse(config.aylien, config.mongodb, config.sources);
 
 var wikianalysis = require('./lib/sources/wiki/analysis_api_input');
-wikianalysis.analyse(config.aylien, config.mongodb, config.sources);
+//wikianalysis.analyse(config.aylien, config.mongodb, config.sources);
 
 
 //var us = require('./lib/upstream.js');
 //var newobj = {"sentiment":.45, "organization":"Maruti", "product":"WagonR", "analysis":"sentiment"};
-//us.aggregate(newobj, config.aylien.watch, config.mongodb);
+//var newobj = {"sentiment":.45, "entity":"Suzuki", "analysis":"sentiment"};
+//us.aggregate(newobj, config.aylien.watch, config.mongodb, config.watch);
 
 
 
