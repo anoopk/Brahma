5996e1af543e1701dde11c9a388e59cb92202f06
Anoop Kumar <anoop_kr@yahoo.com>
2019-02-14 18:59:25 +0530

    Callback added to mongo.store

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 //const config = require('./config.json')
 const urlReader = require('./lib/readers/urlMetaData')
 
-function serve(event){
+function serve(event, callback){
 	const config  = event;
-	console.log(config);
+	const response = {
+		statusCode: 200,
+		body: JSON.stringify('Data Aylienized'),
+	};
+	
 	//Read the metadata 
 	urlReader.nextUrl("input/inputURLList.txt", config.aylien, urlObj => {
 		var aylien = require('./lib/Transformers/transform-sd');
@@ -13,20 +17,22 @@ function serve(event){
 		//Transform the return JSON 
 		aylien.transform(config.aylien, urlObj, (snapshots) => {
 			//Store the results in Mongo
-			mongo.store(snapshots, config.mongodb.url, 'Reviews');		
+			mongo.store(snapshots, config.mongodb.url, 'Reviews', (result) => {});		
 			
 			//Log the Aylien snapshots locally
 			const logger = require('./lib/Stores/logger');		
 			var append = true;
 			logger.store("mongo.db.data", JSON.stringify(snapshots), append);
-			
+
 			//Aggregate the data upstream. As of now this is a naive aggregation as a POC.
 			var us = require('./lib/Aggregators/aggregate-sd.js');
 			us.aggregate(snapshots, config.mongodb, function(snapshot){
-				mongo.store(snapshot, config.mongodb.url, 'Statistics', function(){
+				mongo.store(snapshot, config.mongodb.url, 'Statistics', function(snapshot){
 					console.log("Upstream statistics added.");
+					return snapshot;
 				});			
 			});;
+			
 		});
 	});
 }
