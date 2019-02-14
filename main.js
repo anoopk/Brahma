62319133df2865453a7226385ac7d8ea3331a041
Anoop Kumar <anoop_kr@yahoo.com>
2019-02-14 15:35:09 +0530

    Introduced aggregators (to aggregate data upstream) and transformers(transform ServiceProvider data to model the individual case).

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
 const config = require('./config.json')
 const urlReader = require('./lib/readers/urlMetaData')
 
+//Read the metadata 
 urlReader.nextUrl("input/inputURLList.txt", config.aylien, urlObj => {
-	var aylien = require('./lib/ServiceProviders/analysis');
+	var aylien = require('./lib/Transformers/transform-sd');
 	const mongo = require('./lib/Stores/mongo');
-	var mymongo = new mongo(config.mongodb.url, 'Reviews');
-	aylien.analyse(config.aylien, urlObj, (snapshots) => {
-		mymongo.store(snapshots);		
+	
+	//Send URL text to Aylien API analyse for Classification of subject, Sentiment, Aspect Based Sentiments and Entity Detection.
+	//Transform the return JSON 
+	aylien.transform(config.aylien, urlObj, (snapshots) => {
+		//Store the results in Mongo
+		mongo.store(snapshots, config.mongodb.url, 'Reviews');		
 		
+		//Log the Aylien snapshots locally
 		const logger = require('./lib/Stores/logger');		
 		var append = true;
 		logger.store("mongo.db.data", JSON.stringify(snapshots), append);
 		
-		var us = require('./lib/upstream.js');
+		//Aggregate the data upstream. As of now this is a naive aggregation as a POC.
+		var us = require('./lib/Aggregators/aggregator-sd.js');
 		us.aggregate(snapshots, config.mongodb, function(snapshot){
-			var mymongo = new mongo(config.mongodb.url, "Statistics");
-			mymongo.store(snapshots, function(){
+			mongo.store(snapshots, config.mongodb.url, 'Statistics', function(){
 				console.log("Upstream statistics added.");
 			});			
 		});;
