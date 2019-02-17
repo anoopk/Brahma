3d3761a726515011cbab6b1681b80ce2d2b545ed
anoopk <anoopk@alohatechnology.com>
2019-02-17 15:44:00 +0530

    Configurable values to run each handler as localimport, local lambda or remote lambda on AWS. The last is not yet implemented.

---
+++ executor.js
@@ -0,0 +1,26 @@
+const lambdaLocal = require('lambda-local');
+
+exports.handler = async(event, context) => {
+	if(event.lambdaImport){
+		const ex = require(event.lambdaPath);
+		var snapshots = {};
+		await ex.handler(event, context).then(function(results, err){
+			if(err)console.log("Error ", err);
+			console.log(event.message);
+			snapshots = results;
+		});
+		return snapshots;
+	};		
+	
+	if(event.lambdaLocal){
+		console.log("Mongoising .. ", event);
+		await lambdaLocal.execute({
+			event: event,
+			context: context,
+			lambdaPath: event.lambdaPath,
+			timeoutMs: event.timeOut
+		}).then(function(done) {
+			console.log(event.message);
+		});	
+	}
+}
--- server.js
+++ server.js
@@ -1,44 +1,37 @@
-const lambdaLocal = require('lambda-local');
+const ex = require('./executor.js')
 
 exports.handler = async(event, context) => {
 	const config  = event;
 	//Read the metadata 
-	var aylien = require('./lib/Transformers/sd');
+	//var aylien = require('./lib/Transformers/sd');
 	const mongo = require('./lib/Stores/mongo');
 	//Send URL text to Aylien API analyse for Classification of subject, Sentiment, Aspect Based Sentiments and Entity Detection.
 	//Transform the return JSON 
-	await aylien.handler(config.aylien, {}).then(async function(snapshots){
+	await ex.handler(config.aylien, {}).then(async function(snapshots, error) {
 		config.mongodb.snapshots = snapshots;
 		config.mongodb.db = config.mongodb.databases.analysis;
-		
-		lambdaLocal.execute({
-			event: config.mongodb,
-			lambdaPath: './lib/Stores/mongo',
-			timeoutMs: 3000
-		}).then(function(done) {
-			console.log("Data moved to Mongo");
-		});
-		
+		ex.handler(config.mongodb);
+				
 		//Log the Aylien snaps hots locally
 		const logger = require('./lib/Stores/logger');		
 		var append = true;
 		config.logger.snapshots = snapshots;
 		logger.handler(config.logger, {}).then((result) => { console.log("Local copies of data made.");});
 		
-		var us = require('./lib/Aggregators/sd');
-		config.upstream = {};
-		config.upstream.url = config.mongodb.url;
-		config.upstream.snapshots = snapshots;
+		//var us = require('./lib/Aggregators/sd');
+		//config.upstream = {};
+		//config.upstream.url = config.mongodb.url;
+		//config.upstream.snapshots = snapshots;
 		//config.upstream.db = config.mongodb.databases.analysis;
-		await us.handler(config.mongodb, {}, (result) => { 
-			console.log("Upstream data aggregated.");
-			var mongo = require('./lib/Stores/mongo');
-			config.upstream.snapshots = result;
-			config.upstream.db = config.mongodb.databases.aggregate;
-			mongo.handler(config.upstream, {}, () => {console.log("Data moved to Mongo")});
-		});				
+		//await us.handler(config.mongodb, {}, (result) => { 
+		//	console.log("Upstream data aggregated.");
+		//	var mongo = require('./lib/Stores/mongo');
+		//	config.upstream.snapshots = result;
+		//	config.upstream.db = config.mongodb.databases.aggregate;
+		//	mongo.handler(config.upstream, {}, () => {console.log("Data moved to Mongo")});
+		//});				
 	});
-
+	console.log("Service shutting");
 }
 
 
